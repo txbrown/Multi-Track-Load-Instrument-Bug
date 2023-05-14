3d0ef1fb28821664ff99e356bb0c6ddc2b2bb7bc
Ricardo <ricardo@yonkolevel.com>
2023-05-14 17:44:36 +0100

    Initial Commit

---
+++ Multi Track Load Instrument Bug/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Multi Track Load Instrument Bug
+//
+//  Created by Ricardo Abreu on 14/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Multi Track Load Instrument Bug/Multi_Track_Load_Instrument_BugApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_Track_Load_Instrument_BugApp.swift
+//  Multi Track Load Instrument Bug
+//
+//  Created by Ricardo Abreu on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Multi_Track_Load_Instrument_BugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
